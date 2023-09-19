7d830d3dd6a7f1057cf91ee8bfd1a06518523469
rentezzy <ren2ren4ik@gmail.com>
2023-09-19 20:25:08 +0300

    Add dashboard placeholder

--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -1,5 +1,31 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const Page = () => {
-  return <div className="container">Page</div>;
+  return (
+    <div className="container">
+      <h1 className="font-dela text-[36px] text-primary">
+        Dashboard still in development!
+      </h1>
+      <p className="font-dela text-[24px] text-secondary">But now you can:</p>
+      <ul className="p-2 font-dela text-[16px] text-secondary-darker">
+        <li>
+          <Button variant="link" asChild>
+            <Link href="/questions">Create questions</Link>
+          </Button>
+        </li>
+        <li>
+          <Button variant="link" asChild>
+            <Link href="/questions">Create quizzes</Link>
+          </Button>
+        </li>
+        <li>
+          <Button variant="link" asChild>
+            <Link href="/questions">Complete quizzes</Link>
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Page;
