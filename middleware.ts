ea2e72411e9a03aac346281f0e402d5e241a6a17
rentezzy <ren2ren4ik@gmail.com>
2023-09-20 10:21:37 +0300

    Debug #1

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,9 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest, response: NextResponse) {
   const session = request.cookies.get("session");
 
+  console.log("MIDDLEWARE Session", !!session);
   if (!session) {
+    console.log("MIDDLEWARE login redirect !session");
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
@@ -15,12 +17,13 @@ export async function middleware(request: NextRequest, response: NextResponse) {
   });
 
   if (responseAPI.status !== 200) {
+    console.log("MIDDLEWARE login redirect !200");
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
+  console.log("MIDDLEWARE");
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/overview/:path*","/quizzes/:path*","/questions/:path*"],
+  matcher: ["/overview/:path*", "/quizzes/:path*", "/questions/:path*"],
 };
